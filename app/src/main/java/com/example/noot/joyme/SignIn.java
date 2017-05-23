4a500d912ccdc2975eee37016e1ac9facde24f23
Noot Jettananurak <Noot Jettananurak>
2017-05-23 08:30:52 +0700

    update

--- app/src/main/java/com/example/noot/joyme/SignIn.java
+++ app/src/main/java/com/example/noot/joyme/SignIn.java
@@ -18,6 +18,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class SignIn extends AppCompatActivity {
@@ -27,6 +33,10 @@ public class SignIn extends AppCompatActivity {
     private Button btnSignin, btnSignUp;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("server/saving-data/events");
+
     private ProgressBar progressBar;
 
     @Override
@@ -42,7 +52,7 @@ public class SignIn extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     finish();
-                    startActivity(new Intent(SignIn.this,Profile.class));
+                    startActivity(new Intent(SignIn.this, Profile.class));
                 } else {
 
                 }
@@ -52,7 +62,7 @@ public class SignIn extends AppCompatActivity {
 
     }
 
-    private void initInstances(){
+    private void initInstances() {
         edtUsername = (EditText) findViewById(R.id.edtUsername);
         edtPassword = (EditText) findViewById(R.id.edtPassword);
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
@@ -66,27 +76,27 @@ public class SignIn extends AppCompatActivity {
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
             String username = edtUsername.getText().toString();
             String password = edtPassword.getText().toString();
             progressBar.setVisibility(View.VISIBLE);
 
-            switch (v.getId()){
-                case R.id.btnSignUp :
-                    signUp(username,password);
+            switch (v.getId()) {
+                case R.id.btnSignUp:
+                    signUp(username, password);
                     break;
                 case R.id.btnSignIn:
-                    signIn(username,password);
+                    signIn(username, password);
                     break;
             }
         }
     };
 
     //sign in to application
-    private void signIn(String username, String password){
-        if(!username.isEmpty() && !password.isEmpty()) {
+    private void signIn(final String username, String password) {
+        if (!username.isEmpty() && !password.isEmpty()) {
             mAuth.signInWithEmailAndPassword(username, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -95,35 +105,39 @@ public class SignIn extends AppCompatActivity {
                                 Toast.makeText(SignIn.this, "Sign in fail",
                                         Toast.LENGTH_SHORT).show();
                                 progressBar.setVisibility(View.INVISIBLE);
-                            }else{
+                            } else {
+                                DatabaseReference usersRef = ref.child("users");
+                                DatabaseReference newUserRef = usersRef.child(mAuth.getCurrentUser().getUid());
+                                User user = new User(mAuth.getCurrentUser().getEmail());
+                                Data.getInstance().setUpUser(user);
+                                newUserRef.updateChildren(user.getUserUpdate());
+
                                 progressBar.setVisibility(View.INVISIBLE);
+                                ref.addChildEventListener(childEventListener);
                                 finish();
-                                startActivity(new Intent(SignIn.this,Profile.class));
+                                startActivity(new Intent(SignIn.this, Profile.class));
                             }
 
                             // ...
                         }
                     });
-        }else{
+        } else {
             Toast.makeText(SignIn.this, "Please input username",
                     Toast.LENGTH_SHORT).show();
         }
     }
 
     //go to registration page
-    private void signUp(String username, String password){
+    private void signUp(String username, String password) {
         mAuth.createUserWithEmailAndPassword(username, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Toast.makeText(SignIn.this, "Sign up fail",
                                     Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.INVISIBLE);
-                        }else{
+                        } else {
                             Toast.makeText(SignIn.this, "Account created",
                                     Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.INVISIBLE);
@@ -131,4 +145,31 @@ public class SignIn extends AppCompatActivity {
                     }
                 });
     }
+
+    ChildEventListener childEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+            Data.getInstance().updateUser(dataSnapshot);
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+            Data.getInstance().updateUser(dataSnapshot);
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            Data.getInstance().updateUser(dataSnapshot);
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {
+            Data.getInstance().updateUser(dataSnapshot);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+
+    };
 }
